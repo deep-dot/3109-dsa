e167c880bca3d895eec9a288732b22e395d8c62d
deep-dot <dhillonkuldeep30@gmail.com>
2025-08-28 10:26:48 +1000

    start assignment1

---
+++ assignment1/src/assignment1_task1/Task1.java
@@ -0,0 +1,13 @@
+package assignment1_task1;
+
+public class Task1 {
+	private int[] a = {1,2,3};
+	
+	publict static void nextPermutation(a) {
+		
+	}
+	public class static void main(String[] args) {
+		nextPermutation(a);
+	}
+
+}
